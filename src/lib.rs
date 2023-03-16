6997120fbbb28e4f78b25f51b9a76b6c9039b166
jerry <calljerryli@outlook.com>
2023-03-16 22:30:23 +0800

    update data

--- src/lib.rs
+++ src/lib.rs
@@ -202,3 +202,19 @@ pub async fn write_to_csv(research_report_list: &[ResearchReport]) -> Result<()>
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    #[tokio::test]
+    async fn test_fetch_research_report_by_id() {
+        let client = reqwest::Client::builder()
+            .connect_timeout(std::time::Duration::from_secs(3))
+            .build()
+            .unwrap();
+        let research_report = crate::fetch_research_report_by_id(client, 3922)
+            .await
+            .unwrap();
+        assert_eq!(research_report.title, "2022年数据库云管平台白皮书");
+        assert_ne!(research_report.id, 3939);
+    }
+}
